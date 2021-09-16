7e78b3174325d362ba58b1a9cf618ed656f9da59
Tomek Rozalski <github@tomaszrozalski.pl>
2021-09-16 14:49:27 +0200

    [beverage] Upgrade search by phrase endpoint. It does not work perfectly well, because I cannot create indexes on arrays, but for not it is good enough

--- api/routes/beverages/searchByPhrase.ts
+++ api/routes/beverages/searchByPhrase.ts
@@ -13,31 +13,31 @@ export async function searchByPhrase(ctx: RouterContext) {
 
   const foundArr: BasicsOutput[] = [];
 
-  const value = await beverages
+  await beverages
     .aggregate([
+      { $sort: { added: -1 } },
       {
         $match: {
           $or: [
             // FIND BY:
-            // Badge
-            { badge: { $regex: new RegExp(phrase, "i") } },
             // Name
             { "label.general.name.value": { $regex: new RegExp(phrase, "i") } },
             // Brand name
-            {
-              "label.general.brand.name.value": {
-                $regex: new RegExp(phrase, "i"),
-              },
-            },
+            { "label.general.brand.name.value": { $regex: new RegExp(phrase, "i") } },
             // Series (label / producer)
-            {
-              "label.general.series.value": { $regex: new RegExp(phrase, "i") },
-            },
-            {
-              "producer.general.series.value": {
-                $regex: new RegExp(phrase, "i"),
-              },
-            },
+            { "label.general.series.value": { $regex: new RegExp(phrase, "i") } },
+            { "producer.general.series.value": { $regex: new RegExp(phrase, "i") } },
+            // Style (label / producer / editorial)
+            { "label.brewing.style.value": { $regex: new RegExp(phrase, "i") } },
+            { "producer.brewing.style.value": { $regex: new RegExp(phrase, "i") } },
+            { "editorial.brewing.style.value": { $regex: new RegExp(phrase, "i") } },
+            // Barcode
+            { "label.general.barcode": { $regex: new RegExp(phrase, "i") } },
+            // Tale (label / producer)
+            { "label.general.tale.lead": { $regex: new RegExp(phrase, "i") } },
+            { "label.general.tale.article": { $regex: new RegExp(phrase, "i") } },
+            { "produder.general.tale.lead": { $regex: new RegExp(phrase, "i") } },
+            { "produder.general.tale.article": { $regex: new RegExp(phrase, "i") } },
           ],
         },
       },
