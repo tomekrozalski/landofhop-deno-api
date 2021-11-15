4d3c2b9f80049110240e19bd4dd7a69d6befd6c8
Tomek Rozalski <github@tomaszrozalski.pl>
2021-11-15 18:02:16 +0100

    [beverage] Enhance search by phrase: search by styles

--- api/routes/beverages/searchByPhrase.ts
+++ api/routes/beverages/searchByPhrase.ts
@@ -2,6 +2,7 @@ import { RouterContext } from "oak";
 import format from "date-fns/format";
 
 import { translate } from "/api/utils/translate.ts";
+import { Beverage } from "/models/beverage/details/Beverage.d.ts";
 import { AppLanguage } from "/api/utils/enums/AppLanguage.enum.ts";
 import { DateFormat } from "/api/utils/enums/DateFormat.enum.ts";
 import { beverages } from "/db.ts";
@@ -10,87 +11,112 @@ import type { BasicsOutput } from "./basics/BasicsOutput.d.ts";
 export async function searchByPhrase(ctx: RouterContext) {
   const language = ctx.params.language as AppLanguage;
   const phrase = ctx.params.phrase as string;
+  const styleSearch = phrase.split("style:")[1] ?? null;
 
   const foundArr: BasicsOutput[] = [];
 
-  await beverages
-    .aggregate([
-      { $sort: { added: -1 } },
-      {
-        $match: {
-          $or: [
-            // FIND BY:
-            // Name
-            { "label.general.name.value": { $regex: new RegExp(phrase, "i") } },
-            // Brand name
-            {
-              "label.general.brand.name.value": {
-                $regex: new RegExp(phrase, "i"),
-              },
-            },
-            // Series (label / producer)
-            {
-              "label.general.series.value": { $regex: new RegExp(phrase, "i") },
-            },
-            {
-              "producer.general.series.value": {
-                $regex: new RegExp(phrase, "i"),
-              },
-            },
-            // Style (label / producer / editorial)
-            {
-              "label.brewing.style.value": { $regex: new RegExp(phrase, "i") },
-            },
-            {
-              "producer.brewing.style.value": {
-                $regex: new RegExp(phrase, "i"),
-              },
-            },
-            {
-              "editorial.brewing.style.value": {
-                $regex: new RegExp(phrase, "i"),
-              },
-            },
-            // Barcode
-            { "label.general.barcode": { $regex: new RegExp(phrase, "i") } },
-            // Tale (label / producer)
-            { "label.general.tale.lead": { $regex: new RegExp(phrase, "i") } },
-            {
-              "label.general.tale.article": { $regex: new RegExp(phrase, "i") },
-            },
-            {
-              "produder.general.tale.lead": { $regex: new RegExp(phrase, "i") },
-            },
-            {
-              "produder.general.tale.article": {
-                $regex: new RegExp(phrase, "i"),
-              },
-            },
-          ],
-        },
+  function formatValues({ added, badge, editorial, label, shortId }: Beverage) {
+    console.log("badge", badge);
+    foundArr.push({
+      added: format(new Date(added), DateFormat[language], {}),
+      badge,
+      brand: {
+        badge: label.general.brand.badge,
+        name: translate(label.general.brand.name, language),
       },
-    ])
-    .forEach(({ added, badge, editorial, label, shortId }: any) => {
-      foundArr.push({
-        added: format(new Date(added), DateFormat[language], {}),
-        badge,
-        brand: {
-          badge: label.general.brand.badge,
-          name: translate(label.general.brand.name, language),
-        },
-        containerType: label.container.type,
-        ...(editorial?.photos?.cover &&
-          editorial?.photos?.outlines?.cover && {
-            coverImage: {
-              height: editorial.photos.cover.height,
-              width: editorial.photos.cover.width,
-              outline: editorial.photos.outlines.cover,
-            },
-          }),
-        name: translate(label.general.name, language),
-        shortId,
-      });
+      containerType: label.container.type,
+      ...(editorial?.photos?.cover &&
+        editorial?.photos?.outlines?.cover && {
+          coverImage: {
+            height: editorial.photos.cover.height,
+            width: editorial.photos.cover.width,
+            outline: editorial.photos.outlines.cover,
+          },
+        }),
+      name: translate(label.general.name, language),
+      shortId,
     });
+  }
+
+  if (styleSearch) {
+    await beverages
+      .find(
+        { "editorial.brewing.styleTags.badge": styleSearch },
+        { noCursorTimeout: false }
+      )
+      .forEach(formatValues);
+  } else {
+    await beverages
+      .aggregate([
+        { $sort: { added: -1 } },
+        {
+          $match: {
+            $or: [
+              // FIND BY:
+              // Name
+              {
+                "label.general.name.value": { $regex: new RegExp(phrase, "i") },
+              },
+              // Brand name
+              {
+                "label.general.brand.name.value": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              // Series (label / producer)
+              {
+                "label.general.series.value": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              {
+                "producer.general.series.value": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              // Style (label / producer / editorial)
+              {
+                "label.brewing.style.value": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              {
+                "producer.brewing.style.value": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              {
+                "editorial.brewing.style.value": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              // Barcode
+              { "label.general.barcode": { $regex: new RegExp(phrase, "i") } },
+              // Tale (label / producer)
+              {
+                "label.general.tale.lead": { $regex: new RegExp(phrase, "i") },
+              },
+              {
+                "label.general.tale.article": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              {
+                "produder.general.tale.lead": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+              {
+                "produder.general.tale.article": {
+                  $regex: new RegExp(phrase, "i"),
+                },
+              },
+            ],
+          },
+        },
+      ])
+      .forEach(formatValues);
+  }
 
   ctx.response.body = foundArr;
 }
